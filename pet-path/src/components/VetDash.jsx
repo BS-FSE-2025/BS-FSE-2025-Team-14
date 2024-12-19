2d0d2bb4fb6195e86d7bedec0938f3bf816f328b
נועם מימון <noamma4@ac.sce.ac.il>
2024-12-19 22:34:14 +0200

    הוספת קובץ VetDash.jsx

---
+++ pet-path/src/components/VetDash.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function VatDashboard() {
+  return (
+    <div>
+      <h2>ברוך הבא לדף הוטרינר</h2>
+      <p>כאן תוכל לראות את המידע הרלוונטי לוטרינרים.</p>
+    </div>
+  );
+}
+
+export default VatDashboard;
