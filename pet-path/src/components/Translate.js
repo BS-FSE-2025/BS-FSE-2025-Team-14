204e639c4d7fe701b52f8df6a7010efbe6ef554e
oriohanna <orioh@ac.sce.ac.il>
2024-12-20 11:51:46 +0200

    translate

---
+++ pet-path/src/components/Translate.js
@@ -0,0 +1,102 @@
+// Translate.js
+export const translations = {
+  he: {
+    "About": "אודות",
+    "Walking Safely": "הולכים על בטוח",
+    "What We Offer?": "מה אנחנו מציעים?",
+    "Add Recommendation": "הוסף המלצה",
+    "Existing Recommendations": "המלצות קיימות",
+    "Add Business": "הוסף עסק",
+    "Existing Businesses": "עסקים קיימים",
+    "Who We Are": "מי אנחנו",
+    "Reviews": "כותבים עלינו",
+    "Options": "אפשרויות ",
+    "Recommended Businesses": "עסקים מומלצים",
+    "Reviews and feedback from satisfied users" : "חוות דעת וביקורות ממשתמשים מרוצים.",
+    "Safe walks based on temperature": "טיולים בטוחים בהתאם לטמפרטורה",
+    "Real-time ground safety monitoring": "מעקב אחרי בטיחות הקרקע בזמן אמת",
+    "Displaying recommended maps and routes": "הצגת מפות ומסלולים מומלצים",
+    "Schedule planning based on ground temperature": "תכנון לוז בהתאם לטמפרטורת הקרקע", 
+    "Hadar Trebelsi": "הדר טרבלסי",
+    "Ori Ohana": "אורי אוחנה",
+    "Noam Maimon": "נועם מימון",
+    "Yael Levin": "יעל לוין",
+    "And what else? Join us and discover!": "ומה עוד? הצטרפו אלינו וגלו!",
+    "Join us": "הצטרפות",
+    "PetPath – A smart system for planning safe walks for dogs": "PetPath –מערכת חכמה לתכנון טיולים בטוחים לכלבים",
+    "Helping to choose the ideal time and place for walks and outdoor activities with dogs": "המסייעת לבחור את הזמן והמקום האידיאליים לטיול ופעילויות חוץ עם כלבים",
+    "Considering weather conditions, ground temperature, and radiation levels": "תוך התחשבות בתנאי מזג האוויר, טמפרטורת הקרקע ורמת הקרינה",
+     "Professionals": "בעלי מקצוע",
+    "Business owners who choose PetPath": "בעלי עסק מקצוענים שבוחרים PetPath", 
+    "Name": "שם",
+    "Description": "תיאור",
+    "Choose a role": "בחר תפקיד",
+    "Dog Owner": "בעל כלב",
+    "Veterinarian": "וטרינר",
+    "Dog Walker": "דוג ווקר", 
+    "Group 14": "קבוצה 14",
+    "We are a group of students who believe that with advanced technology, we can improve the quality of life of our four-legged best friends": "אנחנו קבוצה של סטודנטים שמאמינים שעם טכנולוגיה מתקדמת אפשר לשפר את איכות החיים של החברים הכי טובים שלנו על ארבע",
+    "Our vision is to develop smart tools to protect their health and solve everyday challenges": "החזון שלנו הוא לפתח כלים חכמים לשמירה על בריאותם ולפתרון אתגרים יומיומיים",
+    "We work with love for animals and believe that technology can improve the lives of both dogs and humans": "אנחנו פועלים מתוך אהבה לבעלי חיים ומאמינים שהטכנולוגיה יכולה לשפר חיים של כלבים ובני אדם.", 
+  },
+  en: {
+    "אודות": "About",
+    "הולכים על בטוח": "Walking Safely",
+    "מה אנחנו מציעים?": "What We Offer?",
+    "הוסף המלצה": "Add Recommendation",
+    "המלצות קיימות": "Existing Recommendations",
+    "הוסף עסק": "Add Business",
+    "עסקים קיימים": "Existing Businesses",
+    "מי אנחנו": "Who We Are",
+    "כותבים עלינו":"Reviews",
+    "אפשרויות": "Options ",
+    "עסקים מומלצים":"Recommended Businesses",
+    "חוות דעת וביקורות ממשתמשים מרוצים.": "Reviews and feedback from satisfied users",
+    "טיולים בטוחים בהתאם לטמפרטורה": "Safe walks based on temperature",
+    "מעקב אחרי בטיחות הקרקע בזמן אמת": "Real-time ground safety monitoring",
+    "הצגת מפות ומסלולים מומלצים": "Displaying recommended maps and routes",
+    "תכנון לוז בהתאם לטמפרטורת הקרקע": "Schedule planning based on ground temperature",
+    "הדר טרבלסי": "Hadar Trebelsi",
+    "אורי אוחנה": "Ori Ohana",
+    "נועם מימון": "Noam Maimon",
+    "יעל לוין": "Yael Levin",
+    "ומה עוד? הצטרפו אלינו וגלו!": "And what else? Join us and discover!",
+    "הצטרפות": "Join us",
+    "PetPath –מערכת חכמה לתכנון טיולים בטוחים לכלבים": "PetPath – A smart system for planning safe walks for dogs",
+    "המסייעת לבחור את הזמן והמקום האידיאליים לטיול ופעילויות חוץ עם כלבים": "Helping to choose the ideal time and place for walks and outdoor activities with dogs",
+    "תוך התחשבות בתנאי מזג האוויר, טמפרטורת הקרקע ורמת הקרינה": "Considering weather conditions, ground temperature, and radiation levels", 
+    "בעלי מקצוע": "Professionals",
+    "בעלי עסק מקצוענים שבוחרים PetPath": "Business owners who choose PetPath",
+    "שם": "Name",
+    "תיאור": "Description",
+    "בחר תפקיד": "Choose a role",
+    "בעל כלב": "Dog Owner",
+    "וטרינר": "Veterinarian",
+    "דוג ווקר": "Dog Walker", 
+    "קבוצה 14": "Group 14",
+    "אנחנו קבוצה של סטודנטים שמאמינים שעם טכנולוגיה מתקדמת אפשר לשפר את איכות החיים של החברים הכי טובים שלנו על ארבע": "We are a group of students who believe that with advanced technology, we can improve the quality of life of our four-legged best friends",
+    "החזון שלנו הוא לפתח כלים חכמים לשמירה על בריאותם ולפתרון אתגרים יומיומיים": "Our vision is to develop smart tools to protect their health and solve everyday challenges",
+    "אנחנו פועלים מתוך אהבה לבעלי חיים ומאמינים שהטכנולוגיה יכולה לשפר חיים של כלבים ובני אדם.": "We work with love for animals and believe that technology can improve the lives of both dogs and humans",
+  },
+};
+
+// פונקציה להחלפת שפה
+export let currentLanguage = 'he';
+
+export function switchLanguage() {
+  currentLanguage = currentLanguage === 'he' ? 'en' : 'he';
+  updatePageText();
+}
+
+
+export function updatePageText() {
+  const elements = document.body.querySelectorAll('*');
+  elements.forEach((element) => {
+    if (element.children.length === 0 && element.innerText.trim() !== '') {
+      const originalText = element.innerText.trim();
+      if (translations[currentLanguage][originalText]) {
+        element.innerText = translations[currentLanguage][originalText];
+      }
+    }
+  });
+}
